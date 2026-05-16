d884af9eae52e988ad3771127614a780833b0860
Asep Saepul Milah <146751939+asepsaepulmilah2003@users.noreply.github.com>
2026-05-16 16:10:37 +0700

    fix: syamsul slemek

---
+++ android/app/src/main/kotlin/com/penacode/pocketu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.penacode.pocketu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/pocketu/app/pocket_u/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pocketu.app.pocket_u
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
